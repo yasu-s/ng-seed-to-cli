d7e947461b759701190f08585023dec5765ccb41
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2018-06-30 20:27:57 +0900

    初期ファイル追加

---
+++ src/client/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Config } from './shared/config/env.config';
+
+/**
+ * This class represents the main application component.
+ */
+@Component({
+  moduleId: module.id,
+  selector: 'sd-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
+})
+export class AppComponent {
+  constructor() {
+    console.log('Environment config', Config);
+  }
+}
