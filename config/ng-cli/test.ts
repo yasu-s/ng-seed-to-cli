588f0d1f1c4a9960808cf3e542c370b10087c7be
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2018-07-07 20:59:02 +0900

    設定整理

--- config/ng-cli/test.ts
+++ config/ng-cli/test.ts
@@ -15,6 +15,6 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('../client/', true, /\.spec\.ts$/);
+const context = require.context('../../src/client/', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
