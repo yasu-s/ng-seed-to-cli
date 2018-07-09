b33de9395ac763fe023b608412f135e231496cfa
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2018-07-09 18:34:41 +0900

    #4 設定読み込み修正

--- src/client/app/shared/config/env.config.ts
+++ src/client/app/shared/config/env.config.ts
@@ -1,5 +1,9 @@
 import { EnvConfig } from '../../../../../tools/env/env-config.interface';
-import * as DevConfig from '../../../../../tools/env/dev';
+
+async function getConfig() {
+    const config = await import('../../../../../tools/env/dev');
+    return config;
+}
 
 const envJson = '<%= ENV_CONFIG %>';
-export const Config: EnvConfig = (envJson.indexOf('= ENV_CONFIG ') >= 0) ? DevConfig : JSON.parse(envJson);
+export const Config: EnvConfig = (envJson.indexOf('= ENV_CONFIG ') >= 0) ? getConfig() : JSON.parse(envJson);
