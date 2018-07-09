bf8d32fa66f3c0ee11028b300a2a81a8bf7a7a97
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2018-07-09 18:49:26 +0900

    #4 デバッグビルド設定修正

---
+++ src/client/app/shared/config/env.config.dev.ts
@@ -0,0 +1,4 @@
+import { EnvConfig } from '../../../../../tools/env/env-config.interface';
+import * as DevConfig from '../../../../../tools/env/dev';
+
+export const Config: EnvConfig = DevConfig;
--- src/client/app/shared/config/env.config.ts
+++ src/client/app/shared/config/env.config.ts
@@ -1,9 +1,2 @@
 import { EnvConfig } from '../../../../../tools/env/env-config.interface';
-
-async function getConfig() {
-    const config = await import('../../../../../tools/env/dev');
-    return config;
-}
-
-const envJson = '<%= ENV_CONFIG %>';
-export const Config: EnvConfig = (envJson.indexOf('= ENV_CONFIG ') >= 0) ? getConfig() : JSON.parse(envJson);
+export const Config: EnvConfig = JSON.parse('<%= ENV_CONFIG %>');
